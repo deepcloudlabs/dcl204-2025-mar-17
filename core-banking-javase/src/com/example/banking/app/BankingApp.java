9efdc0f225c2c62642b3348941bc31486d8ebaf3
Binnur KURT <deepcloudlabs@gmail.com>
2025-03-17 15:06:08 +0300

    initial commit

--- core-banking-javase/src/com/example/banking/app/BankingApp.java
+++ core-banking-javase/src/com/example/banking/app/BankingApp.java
@@ -8,6 +8,7 @@ public class BankingApp {
 	public static void main(String[] args) {
 		// reference variable -> Heap Object
 		Account acc1 = new Account("tr1", 10_000);
+		acc1.balance = acc1.balance - 10_000_000;
 		System.out.println(acc1.balance);
 	}
 
--- core-banking-javase/src/com/example/banking/domain/Account.java
+++ core-banking-javase/src/com/example/banking/domain/Account.java
@@ -1,14 +1,38 @@
 package com.example.banking.domain;
 
 public class Account {
-   // Attributes: data/property/state
-   private String iban;
-   public double balance;
-   // Behaviors: method	
-   // Constructor
-   public Account(String iban,double balance) {
-	   this.iban = iban;
-	   this.balance = balance;
-   }
-   
+	// Attributes: data/property/state
+	private String iban;
+	private double balance;
+
+	// Behaviors: method
+	// Constructor
+	public Account(String iban, double balance) {
+		this.iban = iban;
+		this.balance = balance;
+	}
+
+	// business method
+	public double withdraw(double amount) {
+		// validation
+		if (amount <= 0.0) {
+			return this.balance;
+		}
+		// business rule
+		if (amount > this.balance) {
+			return this.balance;
+		}
+		// business logic
+		this.balance = this.balance - amount;
+		return this.balance;
+	}
+
+	public double deposit(double amount) {
+		// validation
+		if (amount <= 0.0) {
+			return this.balance;
+		}
+		this.balance = this.balance + amount;
+		return this.balance;
+	}
 }
