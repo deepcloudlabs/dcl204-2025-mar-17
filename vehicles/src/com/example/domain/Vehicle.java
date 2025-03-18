7006c59a357febb4251c54253259e634f94b78a9
Binnur KURT <deepcloudlabs@gmail.com>
2025-03-18 10:24:15 +0300

    initial commit

--- vehicles/src/com/example/domain/Vehicle.java
+++ vehicles/src/com/example/domain/Vehicle.java
@@ -6,6 +6,7 @@ public class Vehicle {
 	private double maxLoad; 
 	private double currentLoad; 
 	// methods: constructor, getter
+	/*
 	public Vehicle() { // no-argument constructor
 		this.maxLoad = 3_000.0;
 		this.currentLoad = 0.0;
@@ -18,7 +19,50 @@ public class Vehicle {
 		this.maxLoad = maxLoad;
 		this.currentLoad = currentLoad;
 	}
-	
+	*/
+	// binocular (dürbün) constructor
+	public Vehicle() { // no-argument constructor
+		this(3_000.0);
+	}
+	public Vehicle(double maxLoad) {
+		this(maxLoad,0.0);
+	}
+	// all-argument constructor
+	public Vehicle(double maxLoad, double currentLoad) {
+		this.maxLoad = maxLoad;
+		this.currentLoad = currentLoad;
+	}
+	public double getMaxLoad() {
+		return maxLoad;
+	}
+	public double getCurrentLoad() {
+		return currentLoad;
+	}
 	//          business methods: load(box), unload(box)
+	public boolean load(double weight) {
+		// validation
+		if(weight <= 0.0) {
+			return false;
+		}
+		// business rule
+		if ((this.currentLoad+weight)>this.maxLoad) {
+			return false;
+		}
+		// this.currentLoad = this.currentLoad + weight;
+		this.currentLoad += weight;
+		return true;
+	}
+	public boolean unload(double weight) {
+		// validation
+		if(weight <= 0.0) {
+			return false;
+		}
+		// business rule
+		if (weight > this.currentLoad) {
+			return false;
+		}
+		this.currentLoad -= weight;
+		return true;
+	}
 	//          toString
 }
