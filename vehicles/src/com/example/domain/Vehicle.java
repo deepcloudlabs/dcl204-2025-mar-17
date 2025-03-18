22a011d1c139446f4b4ff3862ab7beddda02211b
Binnur KURT <deepcloudlabs@gmail.com>
2025-03-18 09:43:37 +0300

    initial commit

---
+++ vehicles/src/com/example/domain/Vehicle.java
@@ -0,0 +1,9 @@
+package com.example.domain;
+
+public class Vehicle {
+	// information hiding
+    // attributes: maxLoad, currentLoad
+	// methods: constructor, getter
+	//          business methods: load(box), unload(box)
+	//          toString
+}
