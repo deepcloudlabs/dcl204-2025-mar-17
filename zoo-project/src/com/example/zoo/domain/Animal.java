c0b59424c9d9f224ff72e4aa07e16dab35767ff5
Binnur KURT <deepcloudlabs@gmail.com>
2025-03-19 14:57:48 +0300

    Update Animal.java

--- zoo-project/src/com/example/zoo/domain/Animal.java
+++ zoo-project/src/com/example/zoo/domain/Animal.java
@@ -1,15 +1,19 @@
 package com.example.zoo.domain;
 
 public abstract class Animal {
-    protected int legs;
+	protected int legs;
 
 	public Animal(int legs) {
 		this.legs = legs;
 	}
-    
+
+	public int getLegs() {
+		return legs;
+	}
+
 	public void walk() { // Concrete
 		System.out.println("Animal with %d legs is running now...".formatted(this.legs));
 	}
-	
+
 	public abstract void eat(); // abstract
 }
