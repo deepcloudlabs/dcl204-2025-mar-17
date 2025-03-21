8a97b2c285980577c3aab2519c0cdf1551225b3b
Binnur KURT <deepcloudlabs@gmail.com>
2025-03-21 09:02:49 +0300

    Update Exercise2.java

--- study-thread-programming/src/com/example/Exercise2.java
+++ study-thread-programming/src/com/example/Exercise2.java
@@ -7,6 +7,9 @@ import java.util.concurrent.FutureTask;
 public class Exercise2 {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		// -Xss128k
+		// Thread.ofPlatform() -> Thread
+		// Thread.ofVirtual()  -> Platform/Developer
 		var task2 = new Task2();
 		var futureTask = new FutureTask<>(task2);
 		Thread t2 = new Thread(futureTask);
