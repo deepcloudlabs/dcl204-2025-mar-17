e8f094409075c7eadab8ae5be67936b42255c4da
Binnur KURT <deepcloudlabs@gmail.com>
2025-03-21 08:27:51 +0300

    initial commit

---
+++ study-thread-programming/src/com/example/Exercise2.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class Exercise2 {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		var task2 = new Task2();
+		var futureTask = new FutureTask<>(task2);
+		Thread t2 = new Thread(futureTask);
+		t2.start();
+		var result = futureTask.get(); // synchronization + result
+		System.out.println(result);
+	}
+
+}
+
+class Task2 implements Callable<Integer> {
+	@Override
+	public Integer call() {
+		System.out.println("Hello Moon!");
+		return 108;
+	}
+
+}
